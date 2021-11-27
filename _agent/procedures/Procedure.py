8e177ea2c7587cdbf39b7c88d75ed2c38762a686
Manuel Salvi <manuel.salvi1911@gmail.com>
2021-11-27 14:31:00 +0100

    debug

--- _agent/procedures/Procedure.py
+++ _agent/procedures/Procedure.py
@@ -19,9 +19,9 @@ class Procedure:
             for step in self._steps:
                 current_step = step
                 print(f"result{result}")
-                bf = step.before(*result)
+                bf = step.before(result)
                 print(f"before{bf}")
-                app = step.apply(*bf)
+                app = step.apply(bf)
                 print(f"app{app}")
                 result = step.after(app)
                 print(f"final_result{result}")
