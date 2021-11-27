b03e05f55d1312172a90ec82b06f9b8f42a235b0
Manuel Salvi <manuel.salvi1911@gmail.com>
2021-11-27 19:27:42 +0100

    session and sysbus separated for sysd

--- _agent/manager/_DbusManager.py
+++ _agent/manager/_DbusManager.py
@@ -3,10 +3,18 @@ from dbus.proxies import ProxyObject
 
 
 def get_sysd_proxy() -> ProxyObject:
-    return get_sys_bus().get_object('org.freedesktop.systemd1', '/org/freedesktop/systemd1')
+    return get_dbus_sys_bus().get_object('org.freedesktop.systemd1', '/org/freedesktop/systemd1')
 
 
-def get_sys_bus() -> dbus.SessionBus:
+def get_dbus_sys_bus() -> dbus.SystemBus:
+    """ Return a connection to the system bus.
+    :returns:
+        `system_bus`:`the system bus connection`
+    """
+    return dbus.SystemBus()
+
+
+def get_dbus_session_bus() -> dbus.SessionBus:
     """ Return a connection to the system bus.
     :returns:
         `system_bus`:`the system bus connection`
@@ -14,5 +22,5 @@ def get_sys_bus() -> dbus.SessionBus:
     return dbus.SessionBus()
 
 
-def get_sysd_object(item) -> ProxyObject:
-    return get_sys_bus().get_object('org.freedesktop.systemd1', item)
+def get_sysd_object(bus, item) -> ProxyObject:
+    return bus().get_object('org.freedesktop.systemd1', item)
