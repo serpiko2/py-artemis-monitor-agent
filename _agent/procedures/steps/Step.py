4151abd0868e7766b9e556124a4c95ad8ae0bf50
Manuel Salvi <manuel.salvi1911@gmail.com>
2021-11-27 14:17:41 +0100

    debug

--- _agent/procedures/steps/Step.py
+++ _agent/procedures/steps/Step.py
@@ -4,11 +4,11 @@ from abc import ABC, abstractmethod
 class Step(ABC):
 
     @abstractmethod
-    def __init__(self, **params):
+    def __init__(self, **kwargs):
         """"""
 
     @abstractmethod
-    def apply(self, **params):
+    def apply(self, **kwargs):
         """ the entrypoint for the step, the scheduler will run it on the event loop,
          it has to be non blocking or you have to delegate it to a secondary thread pool executor
             :param:
@@ -18,10 +18,10 @@ class Step(ABC):
                 `params`:`an object that holds the parameters for the function to run`
         """
 
-    def before(self, **params):
-        print(f"before.self:{self}, params:{params}")
-        return params
+    def before(self, **kwargs):
+        print(f"before.self:{self}, kwargs:{kwargs}")
+        return kwargs
 
-    def after(self, **params):
-        print(f"after.self:{self}, params:{params}")
-        return params
+    def after(self, **kwargs):
+        print(f"after.self:{self}, kwargs:{kwargs}")
+        return kwargs
