652b820e13881866ef66f65d6c301b8ad206605a
Manuel Salvi <manuel.salvi1911@gmail.com>
2021-11-29 16:28:57 +0100

    separated watchdog from event based, kept a shared core

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def main():
     print(f"Glib set as main loop for dbus")
     logger.info(f"Glib set as main loop for dbus")
     SystemBusSysd.get_sys_bus().add_signal_receiver(
-        handler_function=lambda message: print("received signal:", message)
+        handler_function=lambda *args: print("received signal:", *args)
     )
     if 'SYNC' == mode:
         AmqSyncMonitor(monitor_log_path, service_name)
