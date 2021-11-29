bd1a81afd621157e793226fb446349c838c76ee6
Manuel Salvi <manuel.salvi1911@gmail.com>
2021-11-29 12:21:23 +0100

    session and sysbus separated for sysd

--- watchdog/AmqSyncMonitor.py
+++ watchdog/AmqSyncMonitor.py
@@ -1,4 +1,6 @@
-from core.manager import SystemBusSysd
+from datetime import datetime
+
+from watchdog.steps import MmapReadFileStep
 from watchdog.steps.GetPropertiesStep import GetPropertiesStep
 from watchdog.steps.GetServiceStep import GetServiceStep
 from watchdog.steps.RestartUnitStep import RestartUnitStep
@@ -6,7 +8,13 @@ from watchdog.steps.RestartUnitStep import RestartUnitStep
 
 class AmqSyncMonitor:
 
-    stop_sink = SystemBusSysd.get_sysd_manager().connect_to_signal("JobRemoved", lambda m: print(m))
+    @staticmethod
+    def check_from_logs(path):
+        test_file = MmapReadFileStep.mmap_io_find_and_open(filename="test_file.log")
+        ts = datetime.now()
+        print("reading timestamp:", ts)
+        for x in MmapReadFileStep.read_file(test_file, MmapReadFileStep.seek_timestamp, ts):
+            MmapReadFileStep.check_codes(x.message)
 
     @staticmethod
     def check_and_restart(service_name: str):
@@ -14,4 +22,5 @@ class AmqSyncMonitor:
         service_properties = GetPropertiesStep.get_service_properties(unit)
         properties = GetPropertiesStep.get_properties_for_restart(service_properties)
         restart_job = RestartUnitStep.restart_unit(properties, service_name)
+        # reset restart counter
         return restart_job
