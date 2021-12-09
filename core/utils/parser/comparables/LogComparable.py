bb5bb46d5e4885531cc4709e9aa135deb9259b35
Manuel Salvi <manuel.salvi1911@gmail.com>
2021-12-09 23:11:17 +0100

    log groups and comparables

--- core/utils/parser/comparables/LogComparable.py
+++ core/utils/parser/comparables/LogComparable.py
@@ -1,10 +1,38 @@
+from typing import List
+
+from core.utils.parser.comparables.TimestampComparable import TimestampComparable
+from core.utils.parser.logs.LogGroup import LogGroups
+
+
 class LogComparable:
     def __init__(self,
-                 timestamps: () = None,
-                 log_levels: () = None,
-                 logging_classes: () = None,
-                 labels: () = None):
+                 timestamps: TimestampComparable = None,
+                 log_levels: List[str] = None,
+                 logging_classes: List[str] = None,
+                 labels: List[str] = None):
         self.timestamps = timestamps
         self.log_levels = log_levels
         self.logging_classes = logging_classes
         self.labels = labels
+
+
+class LogCompareOperations:
+
+    @staticmethod
+    def is_timestamp_in_range(comparable: LogComparable, group: LogGroups) -> ():
+        raise NotImplemented
+
+    @staticmethod
+    def get_labels_in_message(comparable: LogComparable, group: LogGroups) -> List[str]:
+        found_labels = []
+        for label in comparable.labels:
+            if label.find(group.message):
+                found_labels.append(label)
+        return found_labels
+
+    @staticmethod
+    def is_labels_in_message(group: LogGroups, comparable: LogComparable) -> bool:
+        for label in comparable.labels:
+            if not label.find(group.message):
+                return False
+        return True
