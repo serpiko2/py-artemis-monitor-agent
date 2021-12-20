daeaa5d2e6fd917e8a49a3a98a844c487a781f15
Manuel Salvi <manuel.salvi1911@gmail.com>
2021-12-20 15:44:40 +0100

    logger testt

--- core/Logger.py
+++ core/Logger.py
@@ -4,7 +4,7 @@ import logging
 
 def get_logger(name=None, level=logging.INFO):
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-    handler = handlers.TimedRotatingFileHandler(filename="", when="midnight")
+    handler = handlers.TimedRotatingFileHandler(filename="agent.log", when="midnight")
     handler.setFormatter(formatter)
     logger = logging.getLogger(name)
     logger.setLevel(level)
