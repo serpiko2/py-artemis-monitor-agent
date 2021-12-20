206b497fbfdabfb751500fb0f7eef23513f62918
Manuel Salvi <manuel.salvi1911@gmail.com>
2021-12-20 16:29:06 +0100

    logger test

--- core/Logger.py
+++ core/Logger.py
@@ -20,6 +20,7 @@ class Logger:
 class Loggable:
     def __call__(self, f):
         def wrap(init_self, *args, **kwargs):
-            init_self.logger = Logger.get_logger(init_self.__class__.__name__)
+            print(init_self)
+            init_self = Logger.get_logger(init_self.__class__.__name__)
             f(init_self, *args, **kwargs)
         return wrap
