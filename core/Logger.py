16b4a44e3ccd9af29b149ab2c12b37e954b262e5
Manuel Salvi <manuel.salvi1911@gmail.com>
2021-12-20 16:33:28 +0100

    logger test

--- core/Logger.py
+++ core/Logger.py
@@ -7,7 +7,8 @@ class Logger:
 
     @staticmethod
     def get_logger(name=None, level=logging.INFO):
-        formatter = logging.Formatter('%(asctime)s [%(name)s] [%(threadName)] [%(levelname)] - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+        formatter = logging.Formatter('%(asctime)s [%(name)s] [%(threadName)] [%(levelname)] - %(message)s',
+                                      datefmt='%Y-%m-%d %H:%M:%S')
         handler = handlers.TimedRotatingFileHandler(filename="agent.log", when="midnight")
         handler.setFormatter(formatter)
         logger = logging.getLogger(name)
